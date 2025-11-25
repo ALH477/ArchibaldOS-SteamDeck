47a7b5a2a11729e2bbcde6c6dacc06418f510bbe
Asher <146795551+ALH477@users.noreply.github.com>
2025-11-25 10:47:02 -0800

    Create desktop.nix

---
+++ modules/desktop.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }:
+let
+  basicPackages = with pkgs; [
+    vim kitty wireplumber cava playerctl
+    jetbrains-mono noto-fonts-emoji
+  ];
+in {
+  services.xserver.enable = true;
+  services.desktopManager.plasma6.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+  services.displayManager.defaultSession = "plasma";
+
+  services.displayManager.sddm.settings = {
+    General.Background = "/usr/share/wallpapers/ArchibaldOS/demod-wallpaper.jpg";
+  };
+
+  environment.systemPackages = basicPackages;
+}
